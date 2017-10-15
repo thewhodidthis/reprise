6fe6a3c2292218b18ea4cf4a8a43bf17ffda87cb
Sotiris Bakagiannis <sotirios.bakagiannis@gmail.com>
2017-10-15 19:16:05 +0200

    test: preview object nesting

--- test.js
+++ test.js
@@ -3,6 +3,7 @@ import './index.js'
 const { log } = console
 
 log('%s: %o', '%o', { foo: 'bar' })
+log('%s: %o', '%o', { foo: 'bar', bar: { foo: 'bar' } })
 log('%s: %s %s', '%s', 'Hello', 'world')
 log('%d: %d', '%i', 1001)
 log('%f: %f', '%f', 10.0)
