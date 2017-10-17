5f04ecf0adc85522d2d49e35c0e09d2d20fee965
Sotiris Bakagiannis <sotirios.bakagiannis@gmail.com>
2017-10-17 08:11:36 +0200

    main: fallback when missing arguments

--- index.js
+++ index.js
@@ -5,7 +5,7 @@
   const pimp = JSON.stringify
 
   console.log = (...args) => {
-    const text = args.map(z => (typeof z === 'object' ? pimp(z, null, '\t') : z.toString()))
+    const text = args.map((z = '') => (typeof z === 'object' ? pimp(z, null, '\t') : z.toString()))
 
     if (text.length) {
       const main = text.shift().replace(/%[soOfid]/g, () => text.shift())
