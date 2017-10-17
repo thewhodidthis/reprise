9bce1b517eac8bb26e29fff42f3450f51eb573d3
Sotiris Bakagiannis <sotirios.bakagiannis@gmail.com>
2017-10-17 07:10:39 +0200

    test: cleanup

--- test.js
+++ test.js
@@ -5,5 +5,5 @@ const { log } = console
 log('%s: %o', '%o', { foo: 'bar' })
 log('%s: %o', '%o', { foo: 'bar', bar: { foo: 'bar' } })
 log('%s: %s %s', '%s', 'Hello', 'world')
-log('%d: %d', '%i', 1001)
-log('%f: %f', '%f', 10.0)
+log('%s: %d', '%i', 1001)
+log('%s: %f', '%f', 10.0)
